3b0ea8d91f9b836495441dd0dbffd5380ab47b5e
Luís Conceição <lconceicao@ubiwhere.com>
2021-05-10 15:56:09 +0100

    update

--- pushers/eventcollector/events/event.go
+++ pushers/eventcollector/events/event.go
@@ -53,8 +53,8 @@ func ProcessEvent(e map[string]interface{}) (session models.Session, event model
 		Timestamp: 	   "2021-05-10T14:08:20Z",
 		SourceIP: fmt.Sprintf("%v", e["source-ip"]),
 		Count: 0,
-		Type: "Notice",
-		Priority: "Low",
+		Type: "warning",
+		Priority: "medium",
 		Name: service,
 		Context: service,
 	}
